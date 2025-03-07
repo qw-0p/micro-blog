d576fe6ba2327739108ab7eb71cc1ed1f58ec142
Oleksandr Sarkisov <sarkisov.dev@gmail.com>
2025-03-07 21:18:23 +0200

    add event for comments

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {randomBytes} = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const {content} = req.body;
 
@@ -23,6 +24,15 @@ app.post('/posts/:id/comments', (req, res) => {
     comments.push({id, content});
 
     commentsByPostId[req.params.id] = comments;
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id,
+            content,
+            postId: req.params.id
+        }
+    })
 
     res.status(201).send(comments);
 })
