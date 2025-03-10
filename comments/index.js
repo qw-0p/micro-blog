7d49f868e6135ccff4947494472be31c69d64ff4
Oleksandr Sarkisov <sarkisov.dev@gmail.com>
2025-03-10 12:19:08 +0200

    updating comment content

--- comments/index.js
+++ comments/index.js
@@ -42,8 +42,30 @@ app.post('/posts/:id/comments', async (req, res) => {
     res.status(201).send(comments);
 })
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Received Event', req.body.type);
+
+    const {type, data} = req.body;
+
+    if (type === 'CommentModerated') {
+        const {postId, id, status } = data;
+
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find(comment => comment.id === id);
+
+        comment.status = status;
+
+        await axios.post('http://localhost:4005', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                status,
+                postId,
+                content: comment.content
+            }
+        }).catch(e => console.log(e.message));
+    }
 
     res.send({});
 })
