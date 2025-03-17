335246d7ccdf1d57c331aecce4e6617c7f7f42cb
Oleksandr Sarkisov <sarkisov.dev@gmail.com>
2025-03-17 20:40:05 +0200

    updating service address

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,10 +11,10 @@ app.post('/events', (req, res) => {
 
     events.push(event)
 
-    axios.post('http://localhost:4000/events', event).catch(e => console.log(e.message));
-    axios.post('http://localhost:4001/events', event).catch(e => console.log(e.message));
-    axios.post('http://localhost:4002/events', event).catch(e => console.log(e.message));
-    axios.post('http://localhost:4003/events', event).catch(e => console.log(e.message));
+    axios.post('http://posts-clusterip-srv:4000/events', event).catch(e => console.log(e.message));
+    // axios.post('http://localhost:4001/events', event).catch(e => console.log(e.message));
+    // axios.post('http://localhost:4002/events', event).catch(e => console.log(e.message));
+    // axios.post('http://localhost:4003/events', event).catch(e => console.log(e.message));
 
     res.send({status: 'OK'})
 });
